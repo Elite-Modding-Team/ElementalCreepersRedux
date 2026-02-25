9c25c41546e058d02fca48d18f3457c4774e0bdf
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-02-25 13:23:35 -0600

    Removed unused creative tab

--- src/main/java/mod/emt/elementalcreepers/ElementalCreepersRedux.java
+++ src/main/java/mod/emt/elementalcreepers/ElementalCreepersRedux.java
@@ -6,8 +6,6 @@ import mod.emt.elementalcreepers.config.ECConfigLists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import mod.emt.elementalcreepers.util.ECCreativeTab;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -21,7 +19,6 @@ public class ElementalCreepersRedux {
     public static final String VERSION = "1.0.0";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
-    public static final CreativeTabs TAB = new ECCreativeTab(CreativeTabs.CREATIVE_TAB_ARRAY.length, MOD_ID + ".tab");
 
     @Mod.Instance
     public static ElementalCreepersRedux instance;
